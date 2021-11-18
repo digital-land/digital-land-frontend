db83846268ab6d9b8c21bbe20b33b25147140b40
Colm Britton <colmjude@gmail.com>
2021-11-18 12:05:50 +0000

    Adds dl-filter-group component

--- digital_land_frontend/src/javascripts/digital-land-frontend.js
+++ digital_land_frontend/src/javascripts/digital-land-frontend.js
@@ -1,3 +1,7 @@
 import BackToTop from "../../templates/components/back-to-top/back-to-top";
+import FilterGroupSelectedCounter from "../../templates/components/dl-filter-group/filter-group-selected-counter";
 
-export { BackToTop };
+export { 
+  BackToTop,
+  FilterGroupSelectedCounter
+};
---
+++ digital_land_frontend/templates/components/dl-filter-group/filter-group-selected-counter.js
@@ -0,0 +1,105 @@
+import '../../../../node_modules/govuk-frontend/govuk/vendor/polyfills/Function/prototype/bind'
+
+// ================================
+// Selected counts for filters
+// ================================
+
+function FilterGroupSelectedCounter ($module) {
+  this.$module = $module
+  this.$fieldset = $module.querySelector('fieldset')
+  this.$inputs = this.$fieldset.querySelectorAll('input')
+}
+
+FilterGroupSelectedCounter.prototype.init = function () {
+  var $module = this.$module
+  var $inputs = this.$inputs
+
+  // if no inputs then return
+  if (!$inputs) {
+    return
+  }
+
+  //
+  var boundFetchCountElement = this.fetchCountElement.bind(this)
+  this.countMessage = boundFetchCountElement()
+
+  // if current count is 0 hide the message
+  this.message_is_hidden = false
+  if (this.currentCount === 0) {
+    this.hideCountMessage()
+  }
+
+  // Bind event changes to the textarea
+  var boundChangeEvents = this.bindChangeEvents.bind(this)
+  boundChangeEvents()
+
+  return this
+}
+
+FilterGroupSelectedCounter.prototype.fetchCountElement = function () {
+  var $module = this.$module
+  var countMessage = $module.querySelector('.dl-filter-group__selected-text')
+
+  // if the count message doesn;t exist, create one
+  if (!countMessage) {
+    countMessage = this.createCountElement()
+  }
+
+  this.countElement = countMessage.querySelector('.dl-filter-group__selected-text__count')
+  this.currentCount = parseInt(this.countElement.textContent)
+
+  return countMessage
+}
+
+FilterGroupSelectedCounter.prototype.createCountElement = function () {
+  var $module = this.$module
+  var $summary = $module.querySelector('.dl-filter-group__summary')
+  var firstIcon = $summary.querySelector('svg')
+
+  var countMessage = document.createElement('span')
+  countMessage.classList.add('dl-filter-group__selected-text')
+  countMessage.textContent = ' selected'
+  firstIcon.insertAdjacentElement('beforebegin', countMessage)
+
+  countMessage.insertAdjacentHTML('afterbegin', '<span class="dl-filter-group__selected-text__count">0</span>')
+
+  return countMessage
+}
+
+FilterGroupSelectedCounter.prototype.bindChangeEvents = function () {
+  var $inputs = this.$inputs
+  // console.log(this)
+  $inputs.forEach(input => {
+    input.addEventListener('change', this.updateCount.bind(this))
+  })
+}
+
+FilterGroupSelectedCounter.prototype.updateCount = function () {
+  var $fieldset = this.$fieldset
+  var count = $fieldset.querySelectorAll('input:checked').length
+
+  // if 0 hide
+  if (count === 0) {
+    this.countElement.textContent = 0
+    this.hideCountMessage()
+  } else if (count !== this.currentCount) {
+    // if changed update
+    this.countElement.textContent = count
+    this.showCountMessage()
+  }
+  // if same, do nothing ----
+
+  this.currentCount = count
+}
+
+FilterGroupSelectedCounter.prototype.hideCountMessage = function () {
+  this.countMessage.classList.add('govuk-visually-hidden')
+  this.message_is_hidden = true
+}
+
+FilterGroupSelectedCounter.prototype.showCountMessage = function () {
+  this.countMessage.classList.remove('govuk-visually-hidden')
+  this.message_is_hidden = false
+}
+
+export default FilterGroupSelectedCounter
