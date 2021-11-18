c74601dab193be4adbc4a3b4696838ae5edef9e8
Colm Britton <colmjude@gmail.com>
2021-11-18 12:45:14 +0000

    Adds polyfill item to digital-land-frontend.js

--- digital_land_frontend/src/javascripts/digital-land-frontend.js
+++ digital_land_frontend/src/javascripts/digital-land-frontend.js
@@ -1,7 +1,15 @@
 import BackToTop from "../../templates/components/back-to-top/back-to-top";
 import FilterGroupSelectedCounter from "../../templates/components/dl-filter-group/filter-group-selected-counter";
 
-export { 
+function polyfill (options) {
+  // polyfill for browsers without NodeList forEach method
+  if (window.NodeList && !window.NodeList.prototype.forEach) {
+    window.NodeList.prototype.forEach = window.Array.prototype.forEach
+  }
+}
+
+export {
+  polyfill,
   BackToTop,
   FilterGroupSelectedCounter
 };
