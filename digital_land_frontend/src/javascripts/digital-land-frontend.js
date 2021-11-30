faec30c8afbf95dcadaf8df0a7d8a7c613a17d10
Colm Britton <colmjude@gmail.com>
2021-11-30 11:17:52 +0000

    Removes dl- prefix on template files

--- digital_land_frontend/src/javascripts/digital-land-frontend.js
+++ digital_land_frontend/src/javascripts/digital-land-frontend.js
@@ -1,9 +1,9 @@
 import BackToTop from "../../templates/components/back-to-top/back-to-top";
-import FilterCheckboxes from "../../templates/components/dl-filter-checkboxes/filter-checkboxes";
-import FilterGroupSelectedCounter from "../../templates/components/dl-filter-group/filter-group-selected-counter";
-import HorizontalScrollableTable from "../../templates/components/dl-data-table/horizontal-scrollable-table";
-import ListFilter from "../../templates/components/dl-list-filter/list-filter";
-import SubNavTabs from "../../templates/components/dl-sub-nav-tabs/sub-nav-tabs";
+import FilterCheckboxes from "../../templates/components/filter-checkboxes/filter-checkboxes";
+import FilterGroupSelectedCounter from "../../templates/components/filter-group/filter-group-selected-counter";
+import HorizontalScrollableTable from "../../templates/components/data-table/horizontal-scrollable-table";
+import ListFilter from "../../templates/components/list-filter/list-filter";
+import SubNavTabs from "../../templates/components/sub-nav-tabs/sub-nav-tabs";
 
 function polyfill (options) {
   // polyfill for browsers without NodeList forEach method
--- digital_land_frontend/src/javascripts/digital-land-maps.js
+++ digital_land_frontend/src/javascripts/digital-land-maps.js
@@ -1,10 +1,10 @@
-import Map from '../../templates/components/dl-map/javascripts/map'
-import brownfieldSites from '../../templates/components/dl-map/javascripts/brownfield-land-sites'
-import basicPopup from '../../templates/components/dl-map/javascripts/basic-popup'
-import LayerControls from '../../templates/components/dl-map/javascripts/layer-controls'
-import ZoomControls from '../../templates/components/dl-map/javascripts/zoom-controls'
-import mapUtils from '../../templates/components/dl-map/javascripts/map-utils.js'
-import Permalink from '../../templates/components/dl-map/javascripts/permalink'
+import Map from '../../templates/components/map/javascripts/map'
+import brownfieldSites from '../../templates/components/map/javascripts/brownfield-land-sites'
+import basicPopup from '../../templates/components/map/javascripts/basic-popup'
+import LayerControls from '../../templates/components/map/javascripts/layer-controls'
+import ZoomControls from '../../templates/components/map/javascripts/zoom-controls'
+import mapUtils from '../../templates/components/map/javascripts/map-utils.js'
+import Permalink from '../../templates/components/map/javascripts/permalink'
 
 const utils = mapUtils
 
