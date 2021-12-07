e971fdad7790c08222ff4c786f5e36b4126b8dcb
Colm Britton <colmjude@gmail.com>
2021-12-07 11:19:35 +0000

    Fixes typo

--- package/package-scripts.js
+++ package/package-scripts.js
@@ -23,7 +23,7 @@ module.exports = {
     copy: {
       javascripts: `npx copyfiles "${configPaths.digitalLandFrontendPath}digital-land-frontend/javascripts/**/*.js" ${configPaths.jsOutputPath} -u 4`,
       json: `npx copyfiles "${configPaths.digitalLandFrontendPath}digital-land-frontend/javascripts/**/*.json" ${configPaths.jsOutputPath} -u 4`,
-      images: `npx copyfiles "${configPaths.digitalLandFrontendPath}digital-land-frontend/jimages/**/*.{png,ico}" ${configPaths.imagesOutputPath} -u 4`
+      images: `npx copyfiles "${configPaths.digitalLandFrontendPath}digital-land-frontend/images/**/*.{png,ico}" ${configPaths.imagesOutputPath} -u 4`
     }
   }
 }
