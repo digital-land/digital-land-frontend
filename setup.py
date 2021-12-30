54cf6d8812d402b30eaeef192e3a6c64891c723b
Colm Britton <colmjude@gmail.com>
2021-12-30 10:17:58 +0000

    Bumps package version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for directory in directories_jinja:
 
 setup(
     name="digital-land-frontend",
-    version="0.1.9",
+    version="0.2.0",
     author="Digital land",
     description="Reusable frontend code for digital land services and products",
     license="MIT",
