cffe6c72463964a3b60ddb429d9aa82b6047631b
Colm Britton <colmjude@gmail.com>
2022-03-08 18:24:06 +0000

    Bumps Govuk-frontend version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for directory in directories_jinja:
 
 setup(
     name="digital-land-frontend",
-    version="0.2.0",
+    version="0.2.1",
     author="Digital land",
     description="Reusable frontend code for digital land services and products",
     license="MIT",
