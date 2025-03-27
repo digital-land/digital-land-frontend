2a172489cf94103624422cf8310a21b69338c11a
Adam Shimali <adam.shimali@gmail.com>
2025-03-27 10:26:33 +0000

    chore: update to GOV.UK Frontend 5.9.0 and bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for directory in directories_jinja:
 
 setup(
     name="digital-land-frontend",
-    version="0.2.11",
+    version="1.0.0",
     author="Digital land",
     description="Reusable frontend code for digital land services and products",
     license="MIT",
